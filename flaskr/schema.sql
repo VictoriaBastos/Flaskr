25d0590607263e6a019fe9a983f0dfe2acc7ff0d
victoria bastos <victoria.rezbastos@gmail.com>
2022-07-27 14:36:23 -0700

    create DB tables to store and retrieve data

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,23 +1,17 @@
-import sqlite3
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS post;
 
-import click
-from flask import current_app, g
-from flask.cli import with_appcontext
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL
+);
 
-
-def get_db():
-    if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
-
-    return g.db
-
-
-def close_db(e=None):
-    db = g.pop('db', None)
-
-    if db is not None:
-        db.close()
+CREATE TABLE post (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  author_id INTEGER NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  title TEXT NOT NULL,
+  body TEXT NOT NULL,
+  FOREIGN KEY (author_id) REFERENCES user (id)
+);
